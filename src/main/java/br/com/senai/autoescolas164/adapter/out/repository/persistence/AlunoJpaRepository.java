ea98d0e49f82f0e2841f2b8e87eb8b8f52dd0b07
Otavio Andrade <otavio.sto@gmail.com>
2026-09-12 16:15:19 -0300

    [fix] - corrigindo alunoRepository

--- src/main/java/br/com/senai/autoescolas164/adapter/out/repository/persistence/AlunoJpaRepository.java
+++ src/main/java/br/com/senai/autoescolas164/adapter/out/repository/persistence/AlunoJpaRepository.java
@@ -3,10 +3,11 @@ package br.com.senai.autoescolas164.adapter.out.repository.persistence;
 import br.com.senai.autoescolas164.adapter.out.repository.entity.AlunoEntity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AlunoJpaRepository {
+public interface AlunoJpaRepository extends JpaRepository<AlunoEntity, Long> {
     Page<AlunoEntity> findAllByAtivoTrue(Pageable paginacao);
     boolean existsByIdAndAtivoFalse(Long id);
 
