90e4916060d5a983afed363b73580eba4ae234e6
Otavio Andrade <otavio.sto@gmail.com>
2026-09-12 14:00:40 -0300

    [feat - desacoplar dominio]

--- src/main/java/br/com/senai/autoescolas164/application/core/domain/Aluno.java
+++ src/main/java/br/com/senai/autoescolas164/application/core/domain/Aluno.java
@@ -16,5 +16,64 @@ public class Aluno {
     public Aluno() {
     }
 
-    
+    public Aluno(
+            Long id,
+            String nome,
+            String email,
+            String telefone,
+            String cpf,
+            Endereco endereco,
+            boolean ativo) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.telefone = telefone;
+        this.cpf = cpf;
+        this.endereco = endereco;
+        this.ativo = ativo;
+    }
+
+    public Long getId() { return id; }
+
+    public String getNome() { return nome; }
+
+    public String getEmail() { return email; }
+
+    public String getTelefone() { return telefone; }
+
+    public String getCpf() { return cpf; }
+
+    public Endereco getEndereco() { return endereco; }
+
+    public boolean isAtivo() { return ativo; }
+
+    public void atualizar(
+            String nome,
+            String email,
+            String telefone,
+            Endereco endereco){
+        if (nome != null && !nome.isBlank()) {
+            this.nome = nome;
+        }
+        if (email != null && !email.isBlank()) {
+            this.email = email;
+        }
+        if (telefone != null && !telefone.isBlank()) {
+            this.telefone = telefone;
+        }
+        if (endereco != null) {
+            this.endereco.atualizar(
+                    endereco.getLogradouro(),
+                    endereco.getNumero(),
+                    endereco.getComplemento(),
+                    endereco.getBairro(),
+                    endereco.getCidade(),
+                    endereco.getUf(),
+                    endereco.getCep()
+            );
+        }
+
+    }
+
+    public void excluir() { this.ativo = false; }
 }
