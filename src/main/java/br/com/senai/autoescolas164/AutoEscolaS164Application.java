b8fd6ddcdd385b3091a929de0ee7637f929c0167
Otavio Andrade <otavio.sto@gmail.com>
2026-09-26 13:43:36 -0300

    conf: configuração do cache caffeine

--- src/main/java/br/com/senai/autoescolas164/AutoEscolaS164Application.java
+++ src/main/java/br/com/senai/autoescolas164/AutoEscolaS164Application.java
@@ -2,7 +2,9 @@ package br.com.senai.autoescolas164;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
+@EnableCaching
 @SpringBootApplication
 public class
 AutoEscolaS164Application {
